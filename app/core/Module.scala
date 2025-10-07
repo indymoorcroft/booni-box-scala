b0a7ba24c628f90242aad62fce0eed0a76416c46
indymoorcroft <indigo.moorcroft@hotmail.co.uk>
2025-10-07 16:04:44 +0100

    ensured data seeder runs on startup

---
+++ app/core/Module.scala
@@ -0,0 +1,9 @@
+package core
+
+import com.google.inject.AbstractModule
+
+class Module extends AbstractModule {
+  override def configure(): Unit = {
+    bind(classOf[Startup]).asEagerSingleton()
+  }
+}
